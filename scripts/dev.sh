b608d43b95e8712130832cda030be0318a7505f9
Parshan <parshan.mohebbian@gmail.com>
2026-04-26 19:36:49 +0100

    Remove stale helper scripts

--- scripts/dev.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-set -eu
-
-cd "$(dirname "$0")/.."
-echo "Start the host media runtime in another terminal first:"
-echo "  APRAG_HOST_DATA_DIR=$PWD/data python3 scripts/host_media_runtime.py"
-docker compose --profile vector up --build
